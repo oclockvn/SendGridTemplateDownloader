4dcfb27f57096c2d0d41d2242f80e0213c17b45d
Quang Phan <oclockvn@gmail.com>
2021-10-28 16:07:36 +0700

    [feature] download in batches (#2)
    * ignore result files
    
    * use indexes to fetch template
    
    Co-authored-by: Quang Phan <quang@spiderbox.design>

--- SendGridManager/ExtentionMethods.cs
+++ SendGridManager/ExtentionMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace SendGridManager
@@ -18,6 +19,25 @@ namespace SendGridManager
         public static string ToCsvLine(this IEnumerable<string> cols)
         {
             return string.Join(",", cols);
+        }
+
+        public static bool IsValidFileName(this string s)
+        {
+            return !string.IsNullOrWhiteSpace(s) && s.IndexOfAny(System.IO.Path.GetInvalidFileNameChars()) == -1;
+        }
+
+        public static string CorrectFileName(this string s, char replacement = '-')
+        {
+            var invalidChars = System.IO.Path.GetInvalidFileNameChars();
+            foreach (var c in s)
+            {
+                if (invalidChars.Contains(c))
+                {
+                    s = s.Replace(c, replacement);
+                }
+            }
+
+            return s;
         }
     }
 }
